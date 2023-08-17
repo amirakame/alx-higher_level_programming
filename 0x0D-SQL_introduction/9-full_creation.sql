28693f96c1f22ff626c1f56faa0155a5fbad2ce0
amirakame <amirakamel343@gmail.com>
2023-08-17 15:45:37 -0700

    chool

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates and fills a table second_table with attributes id, name and score. 
+ CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT); 
+  INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "steel", 10); 
+   INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "choan", 3); 
+    INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Max", 14); 
+     INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "chool", 8);
