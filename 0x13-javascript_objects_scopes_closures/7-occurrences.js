d9129ac67b36dac57b3c3007d3c53d170d0488a6
amirakame <amirakamel343@gmail.com>
2023-09-12 15:29:07 -0700

    chool

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {  let nOccurrences = 0;
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       nOccurrences++;
