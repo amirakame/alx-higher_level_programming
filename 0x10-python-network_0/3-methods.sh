7ee7514bc91723b592bdd08649886e2c8094c0a7
amirakame <amirakamel343@gmail.com>
2023-09-28 15:35:47 -0700

    chool

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Displays all HTTP methods the server will accept
-curl -s -X OPTIONS "$1" | grep "Allow: "
+# script that takes in a URL and displays all HTTP methods the server will accept
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
