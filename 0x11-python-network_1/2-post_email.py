dcffd05d8fce22940662cf595a8c7d4453dc6c95
amirakame <amirakamel343@gmail.com>
2023-09-29 14:57:59 -0700

    chool

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 """
 Script that takes in a URL and an email, sends a POST request to the passed
 URL with the email as a parameter, and displays the body of the response
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     data = urlencode(value).encode("ascii")
     req = Request(url, data)
 
-    with urlopen(req) as response: 
+    with urlopen(req) as response:
         print(response.read().decode("utf-8", "replace"))
